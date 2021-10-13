8e4b4eb73802cc5f2c47f30646de23f054525e54
Senja Filipi <sefilipi@microsoft.com>
2021-10-12 21:51:49 -0700

    Sox transform v1

--- audioTransforms.py
+++ audioTransforms.py
@@ -1,5 +1,6 @@
 from torch import nn
 import torch
+from typing import List
 
 class Identity(nn.Module):
   def __init__(self):
@@ -15,4 +16,13 @@ class ExpandDim(nn.Module):
 
   def forward(self, x):
     return torch.unsqueeze(x, dim=self.dim)
-    
+
+class SoxEffectTransform(torch.nn.Module):
+  effects: List[List[str]]
+
+  def __init__(self, effects: List[List[str]]):
+    super().__init__()
+    self.effects = effects
+
+  def forward(self, tensor: torch.Tensor, sample_rate: int):
+    return sox_effects.apply_effects_tensor(tensor, sample_rate, self.effects)
